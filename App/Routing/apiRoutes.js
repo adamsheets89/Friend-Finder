bb15fa60ffd72f13fb897935532facd05af5f3fc
Adam Sheets <adamsheets89@gmail.com>
2018-03-13 21:47:49 -0500

    logic to find new friend works

--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -11,10 +11,49 @@ var getAPI = function (app) {
 
 var postAPI = function (app) {
     app.post("/api/friends", function (req, res) {
-        console.log(req.body);
-        var newFriend = req.body
-        friendsData.push(req.body)
+        var newFriend = req.body;
+        console.log(newFriend);
+
+        console.log("name: " + newFriend.name);
+        console.log("photo: " + newFriend.photo);
+        console.log("answers: " + newFriend.scores);
+
+        //go through friendsData and check each score array against the new user's score array
+
+        var differenceArray = [];
+        for (var i = 0; i < friendsData.length; i++) {
+            var friendCheck = friendsData[i];
+            totalDifference = 0;
+
+            for (var j = 0; j < friendCheck.scores.length; j++)
+                totalDifference += Math.abs(friendCheck.scores[j] - newFriend.scores[j])
+
+            differenceArray.push(totalDifference);
+            console.log("Total difference with " + friendCheck.name + " is " + totalDifference);
+            console.log(differenceArray);
+        }
+
+        var lowestNumber = Math.min(...differenceArray)
+        console.log(lowestNumber);
+
+        var yourMatchObj = {
+            "name": "",
+            "photo": ""
+        }
+ 
+        for (var k = 0; k < differenceArray.length; k++) {
+            if (lowestNumber === differenceArray[k]) {
+                yourMatchObj.name = friendsData[k].name;
+                yourMatchObj.photo = friendsData[k].photo
+            }
+        }
+
+        console.log(yourMatchObj);
+        
+        friendsData.push(newFriend)
         res.json(true)
+
+
     })
 };
 
